8c208d3c28f6bc9cb6484d99436962f4fa0558d8
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2021-01-12 06:34:10 -0500

    Error

--- src/store/reducers.jsx
+++ src/store/reducers.jsx
@@ -1,16 +1,6 @@
-const initialState = (store) => {
-  if (!store) {
-    return { name: '' };
-  } else {
-    return { name: JSON.parse(localStorage.getItem('store')) };
-  }
-};
+import { combineReducers } from 'redux';
+import nameReducer from './nameReducer';
 
-export default (state = initialState, action) => {
-  switch (action.type) {
-    case 'SET_NAME':
-      return { ...state, name: action.payload.name };
-    default:
-      return state;
-  }
-};
+export default combineReducers({
+  name: nameReducer,
+});
